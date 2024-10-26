f12283c1398279922aea89b8b4ccfac15a8fec83
SanekWhite <bro.b98@mail.ru>
2024-10-26 18:25:49 +0700

    Обновление

---
+++ OrderNewUpdate/DataOrder.swift
@@ -0,0 +1,36 @@
+//
+//  data.swift
+//  OrderNewUpdate
+//
+//  Created by Александр Белый on 22.10.2024.
+//
+
+import Foundation
+
+struct Order {
+
+    
+    struct Promocode {
+        let title: String
+        let percent: Int
+        let endDate: Date?
+        let info: String?
+        var active: Bool
+
+    }
+
+    struct Product {
+        let price: Double
+        let title: String
+    }
+
+    var screenTitle: String
+    var promocodes: [Promocode]
+
+    let products: [Product]
+    let paymentDiscount: Double?
+    let baseDiscount: Double?
+
+}
+
+
